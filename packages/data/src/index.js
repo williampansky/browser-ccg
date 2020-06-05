4d7d8774de77adb417d4f006cd9c53274885b87f
William Pansky <williampansky@gmail.com>
2020-06-05 03:53:56 -0500

    enabled esm transpile on data

--- packages/data/src/index.js
+++ packages/data/src/index.js
@@ -1,4 +1,4 @@
 /* eslint-disable no-native-reassign */
 // eslint-disable-next-line no-global-assign
-// require = require('esm')(module /*, options*/);
-// module.exports = require('./main.js');
+require = require('esm')(module /*, options*/);
+module.exports = require('./main.js');
