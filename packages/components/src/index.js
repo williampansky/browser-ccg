9014eb8de3d384bac20e7a8189e054e852bf4a40
William Pansky <williampansky@gmail.com>
2020-06-07 17:10:16 -0500

    renamed mechanic components to match symbols

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -1,6 +1,26 @@
-// export { default as Button } from './Button';
-// export { default as Input } from './Input/Input';
+// ========================================================================
+// GLOBAL => game-related components
 export { default as Card } from './Card';
 export { default as Minion } from './Minion';
+
+// ========================================================================
+// GAME => slots
 export { default as BoardSlot } from './game/slots/BoardSlot';
+
+// ========================================================================
+// GAME => interactions
 export { default as MinionInteraction } from './game/interactions/minions/MinionInteraction';
+
+// ========================================================================
+// GAME => mechanics
+export { default as HasBoon } from './game/mechanics/HasBoon';
+export { default as HasCurse } from './game/mechanics/HasCurse';
+export { default as HasEnergyShield } from './game/mechanics/HasBubble';
+export { default as HasGuardBackground } from './game/mechanics/HasBulwarkBackground';
+export { default as HasGuardForeground } from './game/mechanics/HasBulwarkForeground';
+export { default as HasOnslaught } from './game/mechanics/HasDoubleAttack';
+export { default as HasPoison } from './game/mechanics/HasPoison';
+export { default as IsConcealed } from './game/mechanics/IsConcealed';
+export { default as IsDisabled } from './game/mechanics/IsDisabled';
+export { default as IsElite } from './game/mechanics/IsElite';
+export { default as WillExpire } from './game/mechanics/WillExpire';
