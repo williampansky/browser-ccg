5e897c322376cef39b2fcd8ff1dfb74ca64684ee
William Pansky <williampansky@gmail.com>
2020-06-07 08:42:33 -0500

    tweaked dev importing to split it up

--- packages/dev/src/App.jsx
+++ packages/dev/src/App.jsx
@@ -1,54 +1,10 @@
 import React from 'react';
-import { Card as Component } from '@ccg/components';
-import { CARDS_CORE, CARDS_PRIME } from '@ccg/data';
-import { CARDS, SETS } from '@ccg/images';
-import { getCardBaseImage, getCardFlairImage } from '@ccg/utils';
+import CardDev from './comps/Card.dev';
 
-const App = () => {
-  const CARD = CARDS_CORE['CORE_062'];
-
-  return (
-    <React.Fragment>
-      <div id="app">
-        <Component
-          active={CARD.active}
-          artist={CARD.artist}
-          attack={CARD.attack}
-          collectible={CARD.collectible}
-          cost={CARD.cost}
-          deckBuilder={CARD.deckBuilder}
-          dev={true}
-          elite={CARD.elite}
-          entourage={CARD.entourage}
-          flavor={CARD.flavor}
-          health={CARD.health}
-          howToEarn={CARD.howToEarn}
-          howToEarnGolden={CARD.howToEarnGolden}
-          id={CARD.id}
-          imageBaseSrc={getCardBaseImage(CARD.rarity, CARD.type, CARDS)}
-          imageFlairSrc={getCardFlairImage(CARD.id, CARD.set, SETS)}
-          isGolden={CARD.isGolden}
-          mechanics={CARD.mechanics}
-          name={CARD.name}
-          numberOvercharge={CARD.numberOvercharge}
-          numberPrimary={CARD.numberPrimary}
-          numberRNG={CARD.numberRNG}
-          numberSecondary={CARD.numberSecondary}
-          onClick={CARD.onClick}
-          playContext={CARD.playContext}
-          playRequirements={CARD.playRequirements}
-          playType={CARD.playType}
-          race={CARD.race}
-          rarity={CARD.rarity}
-          set={CARD.set}
-          sounds={CARD.sounds}
-          targetingArrowText={CARD.targetingArrowText}
-          text={CARD.text}
-          type={CARD.type}
-        />
-      </div>
-    </React.Fragment>
-  );
-};
+const App = () => (
+  <React.Fragment>
+    <CardDev />
+  </React.Fragment>
+);
 
 export default App;
