7667ee5282388f8cf61be1e66701fae48d72164d
William Pansky <williampansky@gmail.com>
2020-06-12 10:15:10 -0500

    injected Card component into Hand

--- packages/components/src/Hand/index.jsx
+++ packages/components/src/Hand/index.jsx
@@ -1,17 +1,102 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './styles.module.scss';
+import { getCardBaseImage, getCardFlairImage } from '@ccg/utils';
+import { Card } from '@ccg/components';
 
-const Hand = ({}) => {
+const Hand = ({ cardsInHand, imagesDataCards, imagesDataSets }) => {
   return (
-    <div>
-      <div></div>
+    <div className={styles['hand']} data-file="Hand">
+      {cardsInHand.map((object, i) => {
+        const {
+          active,
+          artist,
+          attack,
+          collectible,
+          cost,
+          deckBuilder,
+          elite,
+          entourage,
+          flavor,
+          health,
+          howToEarn,
+          howToEarnGolden,
+          id,
+          isGolden,
+          mechanics,
+          name,
+          numberOvercharge,
+          numberPrimary,
+          numberRNG,
+          numberSecondary,
+          onClick,
+          playContext,
+          playRequirements,
+          playType,
+          race,
+          rarity,
+          set,
+          sounds,
+          targetingArrowText,
+          text,
+          type,
+          uuid
+        } = object;
+        return (
+          <div className="card__wrapper" key={uuid}>
+            <Card
+              active={active}
+              artist={artist}
+              attack={attack}
+              collectible={collectible}
+              cost={cost}
+              deckBuilder={deckBuilder}
+              elite={elite}
+              entourage={entourage}
+              flavor={flavor}
+              health={health}
+              howToEarn={howToEarn}
+              howToEarnGolden={howToEarnGolden}
+              id={id}
+              imageBaseSrc={getCardBaseImage(rarity, type, imagesDataCards)}
+              imageFlairSrc={getCardFlairImage(id, set, imagesDataSets)}
+              isGolden={isGolden}
+              mechanics={mechanics}
+              name={name}
+              numberOvercharge={numberOvercharge}
+              numberPrimary={numberPrimary}
+              numberRNG={numberRNG}
+              numberSecondary={numberSecondary}
+              onClick={onClick}
+              playContext={playContext}
+              playRequirements={playRequirements}
+              playType={playType}
+              race={race}
+              rarity={rarity}
+              set={set}
+              sounds={sounds}
+              targetingArrowText={targetingArrowText}
+              text={text}
+              type={type}
+              uuid={uuid}
+            />
+          </div>
+        );
+      })}
     </div>
   );
 };
 
-Hand.propTypes = {};
+Hand.propTypes = {
+  cardsInHand: PropTypes.array,
+  imagesDataCards: PropTypes.object,
+  imagesDataSets: PropTypes.object
+};
 
-Hand.defaultProps = {};
+Hand.defaultProps = {
+  cardsInHand: [],
+  imagesDataCards: {},
+  imagesDataSets: {}
+};
 
 export default Hand;
--- packages/dev/src/components/Hand.dev.jsx
+++ packages/dev/src/components/Hand.dev.jsx
@@ -2,7 +2,8 @@
 import React from 'react';
 import { Hand } from '@ccg/components';
 import { ABILITIES, CARDS_DATABASE } from '@ccg/data';
-import { getHeroImage, getHeroName } from '@ccg/utils';
+import { CARDS, SETS } from '@ccg/images';
+import { getCardByID } from '@ccg/utils';
 import {
   ABILITIES_ICON,
   ABILITIES_ICON_CLOSE,
@@ -10,55 +11,16 @@ import {
   PLACEHOLDER_IMAGE
 } from '@ccg/images';
 
-export default function HeroDev() {
-  const HERO = 'ZEUS';
-  const HERO_SYMBOL = `%HERO_${HERO}%`;
-  const PLAYER_DECK = [
-    {
-      ...CARDS_DATABASE['CORE_001'.replace(' ', '')],
-      _id: 'CORE_001',
-      _amount: 0
-    },
-    {
-      ...CARDS_DATABASE['CORE_002'.replace(' ', '')],
-      _id: 'CORE_002',
-      _amount: 2
-    },
-    {
-      ...CARDS_DATABASE['CORE_020'.replace(' ', '')],
-      _id: 'CORE_020',
-      _amount: 2
-    },
-    {
-      ...CARDS_DATABASE['CORE_030'.replace(' ', '')],
-      _id: 'CORE_030',
-      _amount: 2
-    },
-    {
-      ...CARDS_DATABASE['CORE_022'.replace(' ', '')],
-      _id: 'CORE_022',
-      _amount: 1
-    },
-    {
-      ...CARDS_DATABASE['CORE_040'.replace(' ', '')],
-      _id: 'CORE_040',
-      _amount: 2
-    },
-    {
-      ...CARDS_DATABASE['CORE_080'.replace(' ', '')],
-      _id: 'CORE_080',
-      _amount: 2
-    },
-    {
-      ...CARDS_DATABASE['CORE_044'.replace(' ', '')],
-      _id: 'CORE_044',
-      _amount: 2
-    }
-  ].sort((a, b) => a.cost - b.cost);
-
+export default function HandDev() {
+  const CARD_01 = getCardByID('CORE_001');
+  const CARDS_ARRAY = [CARD_01];
   return (
     <div id="app" style={{ justifyContent: 'flex-end' }}>
-      <Hand />
+      <Hand
+        cardsInHand={CARDS_ARRAY}
+        imagesDataCards={CARDS}
+        imagesDataSets={SETS}
+      />
     </div>
   );
 }
