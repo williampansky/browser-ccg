cd3d5dc83045e3eea106c6fb9c1b6a8febb55ca0
William Pansky <williampansky@gmail.com>
2020-06-12 12:35:38 -0500

    added ability to open hand tray

--- packages/components/src/Hand/index.jsx
+++ packages/components/src/Hand/index.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './styles.module.scss';
 import { getCardBaseImage, getCardFlairImage } from '@ccg/utils';
 import { CardInteractionLayer } from '@ccg/components';
 
 const Hand = ({ cardsInHand, imagesDataCards, imagesDataSets }) => {
+  const [trayIsExpanded, setTrayIsExpanded] = useState(false);
+
+  const handleHandTrayClick = useCallback(
+    event => {
+      event.preventDefault();
+      !trayIsExpanded ? setTrayIsExpanded(true) : setTrayIsExpanded(false);
+    },
+    [trayIsExpanded, setTrayIsExpanded]
+  );
+
   return (
-    <div className={styles['hand']} data-file="Hand">
+    <div
+      className={[
+        styles['hand'],
+        trayIsExpanded ? styles['hand--is-expanded'] : ''
+      ].join(' ')}
+      data-file="Hand"
+      onClick={e => handleHandTrayClick(e)}
+      onKeyPress={e => handleHandTrayClick(e)}
+      role="button"
+      tabIndex={0}
+    >
       {cardsInHand.map((object, index) => {
         const { id, rarity, set, type, uuid } = object;
         return (
@@ -16,7 +36,7 @@ const Hand = ({ cardsInHand, imagesDataCards, imagesDataSets }) => {
               cardImageBaseSrc={getCardBaseImage(rarity, type, imagesDataCards)}
               cardImageFlairSrc={getCardFlairImage(id, set, imagesDataSets)}
               index={index}
-              isPlayable={index === 0 ? false : true}
+              isPlayable={false}
               isSelected={false}
             />
           </div>
@@ -29,13 +49,15 @@ const Hand = ({ cardsInHand, imagesDataCards, imagesDataSets }) => {
 Hand.propTypes = {
   cardsInHand: PropTypes.array,
   imagesDataCards: PropTypes.object.isRequired,
-  imagesDataSets: PropTypes.object.isRequired
+  imagesDataSets: PropTypes.object.isRequired,
+  trayIsExpanded: PropTypes.bool.isRequired
 };
 
 Hand.defaultProps = {
   cardsInHand: [],
   imagesDataCards: {},
-  imagesDataSets: {}
+  imagesDataSets: {},
+  trayIsExpanded: true
 };
 
 export default Hand;
