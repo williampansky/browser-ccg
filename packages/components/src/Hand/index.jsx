9864a89aa03b0e765b967e633074b6818818e862
William Pansky <williampansky@gmail.com>
2020-06-29 22:37:20 -0500

    added invisible div over card-in-hand items to assist in smooth and UX-focused interaction

--- packages/components/src/Hand/index.jsx
+++ packages/components/src/Hand/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import DesktopHand from './DesktopHandSprings';
+import DesktopHand from './DesktopHandSpringsHover';
 import MobileHand from './MobileHand';
 
 const Hand = props => {
