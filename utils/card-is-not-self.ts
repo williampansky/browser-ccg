3d9225fb8a8625627100b9fa3cda4f541a5ae034
William Pansky <williampansky@gmail.com>
2022-12-01 16:27:47 -0600

    added comments to new util

--- utils/card-is-not-self.ts
+++ utils/card-is-not-self.ts
@@ -1,5 +1,23 @@
-import { Card } from "../types";
+import { Card } from '../types';
 
+/**
+ * Used to determine if the `card1` and `card2` params are not the
+ * same instance using a `uuid` check.
+ *
+ * ```ts
+ * const { incomingCard } = props;
+ * G.zones.forEach(z => {
+ *  z.sides[playerId].forEach(cardSlot => {
+ *    if (cardIsNotSelf(cardSlot, incomingCard)) {
+ *      // ... some code executed here
+ *    }
+ *  })
+ * })
+ *
+ * ```
+ *
+ * @example cardIsNotSelf(cardInLoop, cardThatWasPlayed)
+ */
 export default function cardIsNotSelf(card1: Card, card2: Card): boolean {
   return card1.uuid !== card2.uuid;
 }
